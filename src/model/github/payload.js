acbc47ee0c7ada4bef7282dd814d5567c7c98fad
snakazawa <nakazawa.tms0314@gmail.com>
2017-07-01 03:10:51 +0900

    refactor payload

--- src/model/github/payload.js
+++ src/model/github/payload.js
@@ -1,13 +1,47 @@
 // @flow
 
-export type Payload$User = {login: string, url: string, html_url: string, avatar_url: string};
+export type Payload$User = {
+    login: string,
+    id: number,
+    url: string,
+    html_url: string,
+    avatar_url: string
+};
+
 export type Payload$Sender = Payload$User;
-export type Payload$Repository = {name: string, full_name: string, url: string, html_url: string, owner: Payload$User};
-export type Payload$Label = {url: string, name: string, color: string}
-export type Payload$Milestone = {url: string, html_url: string, number: number, title: string, state: string, description: string, due_on: string};
+
+export type Payload$Repository = {
+    id: number,
+    name: string,
+    full_name: string,
+    url: string,
+    html_url: string,
+    owner: Payload$User,
+    description: string
+};
+
+export type Payload$Label = {
+    url: string,
+    name: string,
+    color: string
+};
+
+export type Payload$Milestone = {
+    url: string,
+    html_url: string,
+    id: number,
+    number: number,
+    title: string,
+    state: string,
+    description?: string,
+    creator: Payload$User,
+    due_on?: string
+};
+
 export type Payload$Issue = {
     url: string,
     html_url: string,
+    id: number,
     number: number,
     title: string,
     body: string,
@@ -17,12 +51,14 @@ export type Payload$Issue = {
     assignees: Array<Payload$User>,
     milestone?: Payload$Milestone
 };
+
 export type Payload$Comment = {
     url: string,
     html_url: string,
     user: Payload$User,
     body: string
 };
+
 export type Payload$Page = {
     page_name: string,
     title: string,
@@ -30,6 +66,7 @@ export type Payload$Page = {
     sha: string,
     html_url: string
 };
+
 export type Payload$Project = {
     name: string,
     body: string,
@@ -38,13 +75,15 @@ export type Payload$Project = {
     creater: Payload$User,
     html_url: string
 };
+
 export type Payload$ProjectColumn = {
     id: number,
     name: string,
     url: string,
     project_url: string,
     cards_url: string
-}
+};
+
 export type Payload$ProjectCard = {
     url: string,
     column_url: string,
@@ -52,7 +91,7 @@ export type Payload$ProjectCard = {
     id: number,
     note?: string,
     creator: Payload$User
-}
+};
 
 export class Payload {
     repository: Payload$Repository;
